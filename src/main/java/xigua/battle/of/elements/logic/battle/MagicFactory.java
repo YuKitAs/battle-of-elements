f2bdefac82f0d67983c7a0f36e101a37c83ad0f6
xiGUAwanOU <xiguawanou@gmail.com>
2016-09-18 22:39:21 +0200

    Added a verification to the MagicFactory. Red unit test result is expected.

--- src/main/java/xigua/battle/of/elements/logic/battle/MagicFactory.java
+++ src/main/java/xigua/battle/of/elements/logic/battle/MagicFactory.java
@@ -11,6 +11,8 @@ public class MagicFactory {
         //   2. main element;
         //   3. magic level;
         //   4. end of the magic.
+        verifyElementBank(summonedElementBank);
+
         if (summonedElementBank.getCurrentSize() < 4) {
             return Magic.EMPTY;
         }
@@ -23,5 +25,23 @@ public class MagicFactory {
 
     public Magic buildNonTypedMagic(Element effectElement, int primaryLevel) {
         return new Magic(effectElement, Element.NONE, primaryLevel, 0);
+    }
+
+    private void verifyElementBank(SummonedElementBank summonedElementBank) {
+        int currentSize = summonedElementBank.getCurrentSize();
+        Element effectElement = summonedElementBank.getFirst();
+
+        if (currentSize < 3) {
+            throw new RuntimeException("Too few elements for casting magic.");
+        }
+
+        if (summonedElementBank.getLast().getDestructedElement() != effectElement) {
+            throw new RuntimeException("Element bank is not ended properly.");
+        }
+
+        if (summonedElementBank.getElementLevelCount(effectElement) + summonedElementBank.getElementLevelCount(
+                (effectElement.getDestructedElement())) + 3 != currentSize) {
+            throw new RuntimeException("Invalid element bank for casting magic.");
+        }
     }
 }
