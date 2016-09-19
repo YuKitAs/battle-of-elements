0cf1291fbcda79c5d298ec951b96b54326cb27df
YuKitAs <moncher.merci@gmail.com>
2016-09-19 22:14:42 +0200

    Unit test for ElementFactory.

--- src/test/java/xigua/battle/of/elements/logic/battle/ElementFactoryTest.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/ElementFactoryTest.java
@@ -1,11 +1,24 @@
 package xigua.battle.of.elements.logic.battle;
 
 import org.junit.Test;
+import xigua.battle.of.elements.model.battle.Element;
+import xigua.battle.of.elements.model.battle.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ElementFactoryTest {
+    private static final long SEED = 10000;
+
     @Test
-    public void getElement() {
+    public void getElementInBalancedEnv_ReturnSameElementsWithSameSeed() {
+        ElementFactory elementFactory = new ElementFactory(Environment.BALANCED, SEED);
 
+        assertThat(elementFactory.getElement()).isEqualTo(Element.FIRE);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.WATER);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.WOOD);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.WATER);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.WOOD);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.WATER);
+        assertThat(elementFactory.getElement()).isEqualTo(Element.FIRE);
     }
-
 }
