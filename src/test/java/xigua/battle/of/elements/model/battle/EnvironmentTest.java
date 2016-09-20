0f184d8542a123ac6199250bc36375f3ccd0d058
YuKitAs <moncher.merci@gmail.com>
2016-09-20 22:11:32 +0200

    Modified unit test for Environment.

--- src/test/java/xigua/battle/of/elements/model/battle/EnvironmentTest.java
+++ src/test/java/xigua/battle/of/elements/model/battle/EnvironmentTest.java
@@ -2,22 +2,16 @@ package xigua.battle.of.elements.model.battle;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvironmentTest {
     @Test
     public void getElementProportions_WithCorrectProportions() {
-        List<Element> balancedElementProp = Arrays.asList(Element.FIRE, Element.WATER, Element.WOOD);
-        List<Element> moreFireElementProp = Arrays.asList(Element.FIRE, Element.FIRE, Element.FIRE, Element.WATER,
-                Element.WATER, Element.WOOD, Element.WOOD);
-        List<Element> fireDominantElementProp = Arrays.asList(Element.FIRE, Element.FIRE, Element.WATER, Element.WOOD);
-
-        assertThat(Environment.BALANCED.getElementProportions()).containsExactlyElementsOf(balancedElementProp);
-        assertThat(Environment.MORE_FIRE.getElementProportions()).containsExactlyElementsOf(moreFireElementProp);
-        assertThat(Environment.FIRE_DOMINANT.getElementProportions()).containsExactlyElementsOf
-                (fireDominantElementProp);
+        assertThat(Environment.BALANCED.getElementProportions()).containsExactly(Element.FIRE, Element.WATER, Element
+                .WOOD);
+        assertThat(Environment.MORE_FIRE.getElementProportions()).containsExactly(Element.FIRE, Element.FIRE, Element
+                .FIRE, Element.WATER, Element.WATER, Element.WOOD, Element.WOOD);
+        assertThat(Environment.FIRE_DOMINANT.getElementProportions()).containsExactly(Element.FIRE, Element.FIRE,
+                Element.WATER, Element.WOOD);
     }
 }
