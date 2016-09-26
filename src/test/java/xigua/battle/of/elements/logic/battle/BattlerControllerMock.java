4b46b6f307f7e518a2b539c2135c7d595a55c138
YuKitAs <moncher.merci@gmail.com>
2016-09-27 00:22:17 +0200

    Implemented and tested BattlerControllerMock.

--- src/test/java/xigua/battle/of/elements/logic/battle/BattlerControllerMock.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/BattlerControllerMock.java
@@ -1,5 +1,61 @@
 package xigua.battle.of.elements.logic.battle;
 
-public class BattlerControllerMock {
+import xigua.battle.of.elements.logic.battle.actions.Action;
+import xigua.battle.of.elements.model.battle.ElementConsumeChoice;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class BattlerControllerMock implements BattlerController {
+    private List<ElementConsumeChoice> elementChoicesReceived = new ArrayList<>();
+    private Queue<ElementConsumeChoice> elementConsumeChoices = new LinkedList<>();
+
+    private Set<Action> actionsReceived = new LinkedHashSet<>();
+    private Queue<Action> actions = new LinkedList<>();
+
+    private Set<String> enemyNamesReceived = new LinkedHashSet<>();
+    private Queue<String> targets = new LinkedList<>();
+
+    @Override
+    public ElementConsumeChoice chooseSummonedElement(List<ElementConsumeChoice> elementChoices) {
+        elementChoicesReceived = elementChoices;
+
+        elementChoices.forEach(choice -> elementConsumeChoices.add(choice));
+
+        return elementConsumeChoices.poll();
+    }
+
+    @Override
+    public Action chooseAction(Set<Action> actions) {
+        actionsReceived = actions;
+
+        actions.forEach(action -> this.actions.add(action));
+
+        return this.actions.poll();
+    }
+
+    @Override
+    public String chooseTarget(Set<String> enemyNames) {
+        enemyNamesReceived = enemyNames;
+
+        enemyNames.forEach(name -> targets.add(name));
+
+        return targets.poll();
+    }
+
+    public List<ElementConsumeChoice> getElementChoicesReceived() {
+        return elementChoicesReceived;
+    }
+
+    public Set<Action> getActionsReceived() {
+        return actionsReceived;
+    }
+
+    public Set<String> getEnemyNamesReceived() {
+        return enemyNamesReceived;
+    }
 }
--- src/test/java/xigua/battle/of/elements/logic/battle/BattlerObserverMock.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/BattlerObserverMock.java
@@ -1,5 +1,41 @@
 package xigua.battle.of.elements.logic.battle;
 
-public class BattlerObserverMock {
+import xigua.battle.of.elements.model.battle.BattleField;
+import xigua.battle.of.elements.model.battle.ElementConsumeChoice;
+import xigua.battle.of.elements.model.battle.actions.ElementUsage;
+import xigua.battle.of.elements.model.battle.actions.Magic;
+import xigua.battle.of.elements.model.battle.battler.Battler;
 
+public class BattlerObserverMock implements BattlerObserver {
+
+    @Override
+    public void notifyBattleStarted(Battler currentBattler, BattleField battleField) {
+
+    }
+
+    @Override
+    public void notifyRoundStarted(Battler currentBattler, BattleField battleField) {
+
+    }
+
+    @Override
+    public void notifyRoundEnded(Battler currentBattler, BattleField battleField) {
+
+    }
+
+    @Override
+    public void notifyAfterChoseElement(ElementConsumeChoice choice, Battler currentBattler, BattleField battleField) {
+
+    }
+
+    @Override
+    public void notifyAfterBuiltMagic(Magic magic, Battler currentBattler, BattleField battleField) {
+
+    }
+
+    @Override
+    public void notifyAppliedMagic(ElementUsage usage, Battler targetBefore, Battler targetAfter, Battler
+            currentBattler, BattleField battleField) {
+
+    }
 }
