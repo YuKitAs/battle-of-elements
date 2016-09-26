431ee1eaa426e261d2b6300adec52660c633be07
YuKitAs <moncher.merci@gmail.com>
2016-09-26 21:10:53 +0200

    Unit test for CommonRules.

--- src/test/java/xigua/battle/of/elements/logic/battle/actions/CommonRulesTest.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/actions/CommonRulesTest.java
@@ -1,4 +1,17 @@
 package xigua.battle.of.elements.logic.battle.actions;
 
+import org.junit.Test;
+import xigua.battle.of.elements.model.battle.Element;
+import xigua.battle.of.elements.model.battle.actions.ElementUsage;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 public class CommonRulesTest {
+    @Test
+    public void getElementUsage_WithCorrectElementUsage() {
+        assertThat(CommonRules.getElementUsage(Element.FIRE)).isEqualTo(ElementUsage.ATTACK);
+        assertThat(CommonRules.getElementUsage(Element.WATER)).isEqualTo(ElementUsage.DEFEND);
+        assertThat(CommonRules.getElementUsage(Element.WOOD)).isEqualTo(ElementUsage.HEAL);
+        assertThat(CommonRules.getElementUsage(Element.NONE)).isEqualTo(ElementUsage.NONE);
+    }
 }
