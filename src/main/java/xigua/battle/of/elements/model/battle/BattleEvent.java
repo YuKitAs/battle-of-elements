ebe2ccb7c25c58df80c0322495a59b39e32500f6
xiGUAwanOU <xiguawanou@gmail.com>
2016-09-26 23:50:11 +0200

    Modified the BattleEvent class to enum.

--- src/main/java/xigua/battle/of/elements/model/battle/BattleEvent.java
+++ src/main/java/xigua/battle/of/elements/model/battle/BattleEvent.java
@@ -5,17 +5,10 @@ import xigua.battle.of.elements.utility.DeepCopy;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BattleEvent {
-    private final String reciever;
+public enum BattleEvent {
+    BATTLE_START;
+
     private final Map<String, byte[]> attributes = new HashMap<>();
-
-    public BattleEvent(String reciever) {
-        this.reciever = reciever;
-    }
-
-    public String getReciever() {
-        return reciever;
-    }
 
     public void putAttribute(String key, Object value) {
         attributes.put(key, DeepCopy.toBytes(value));
