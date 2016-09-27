7e7d1d1852f9cb5b7f16f8d27b05ba1803a2aaab
YuKitAs <moncher.merci@gmail.com>
2016-09-27 02:14:30 +0200

    Implemented BattlerObserverMock and tested BattleHelper.

--- src/test/java/xigua/battle/of/elements/logic/battle/BattleHelperTest.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/BattleHelperTest.java
@@ -1,8 +1,18 @@
 package xigua.battle.of.elements.logic.battle;
 
 import org.junit.Test;
+import xigua.battle.of.elements.model.Event;
+import xigua.battle.of.elements.model.EventType;
+import xigua.battle.of.elements.model.IntWithMax;
+import xigua.battle.of.elements.model.battle.BattleField;
 import xigua.battle.of.elements.model.battle.Element;
 import xigua.battle.of.elements.model.battle.ElementUsage;
+import xigua.battle.of.elements.model.battle.Environment;
+import xigua.battle.of.elements.model.battle.FreeElementBank;
+import xigua.battle.of.elements.model.battle.battler.Battler;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -13,5 +23,23 @@ public class BattleHelperTest {
         assertThat(BattleHelper.getElementUsage(Element.WATER)).isEqualTo(ElementUsage.DEFEND);
         assertThat(BattleHelper.getElementUsage(Element.WOOD)).isEqualTo(ElementUsage.HEAL);
         assertThat(BattleHelper.getElementUsage(Element.NONE)).isEqualTo(ElementUsage.NONE);
+    }
+
+    @Test
+    public void notify_AllBattlersNotified() {
+        BattlerObserverMock battlerObserverMock = new BattlerObserverMock();
+
+        Set<Battler> battlers = new HashSet<>();
+        battlers.add(new Battler("John Doe", true, true, 10, 10, 10, new IntWithMax(10, 20), new IntWithMax(10, 20),
+                5, new FreeElementBank(5), new BattlerControllerMock(), battlerObserverMock));
+
+        BattleField battleField = new BattleField(battlers, Environment.BALANCED);
+        BattleHelper battleHelper = new BattleHelper(battleField);
+
+        battleHelper.notifyAllBattlers(new Event(EventType.BATTLE_ACTION_START));
+        battleHelper.notifyAllBattlers(new Event(EventType.BATTLE_ACTION_END));
+
+        assertThat(battlerObserverMock.getEventReceived().get(0).getType()).isEqualTo(EventType.BATTLE_ACTION_START);
+        assertThat(battlerObserverMock.getEventReceived().get(1).getType()).isEqualTo(EventType.BATTLE_ACTION_END);
     }
 }
--- src/test/java/xigua/battle/of/elements/logic/battle/BattlerObserverMock.java
+++ src/test/java/xigua/battle/of/elements/logic/battle/BattlerObserverMock.java
@@ -3,15 +3,23 @@ package xigua.battle.of.elements.logic.battle;
 import xigua.battle.of.elements.model.Event;
 import xigua.battle.of.elements.model.battle.battler.Battler;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BattlerObserverMock implements BattlerObserver {
+    private final List<Event> eventReceived = new LinkedList<>();
 
     @Override
     public void setBattler(Battler battler) {
-
+        // Simply do nothing here.
     }
 
     @Override
     public void notify(Event event) {
+        eventReceived.add(event);
+    }
 
+    public List<Event> getEventReceived() {
+        return eventReceived;
     }
 }
